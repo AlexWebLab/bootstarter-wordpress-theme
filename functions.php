2ad2668a8a97267e5bf9326e16ad3adf8b14fc86
Alessandro <info@alexweblab.com>
2016-11-28 18:26:16 +0800

    added woocommerce support

--- functions.php
+++ functions.php
@@ -28,6 +28,12 @@ require get_template_directory() . '/inc/wp_bootstrap_navwalker.php';
 // Disable Woocommerce Default Styling
 add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
 
+// Disable Woocommerce Default Styling
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'woocommerce_support' );
+
 // Custom CSS for WYSIWYG editor
 function custom_editor_style() {
 	add_editor_style(get_template_directory_uri().'/css/style-wysiwyg-editor.css');
