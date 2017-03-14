db98acc288e4cf3fc27a649b158efff519712908
Alessandro <info@alexweblab.com>
2017-03-14 12:37:49 +0800

    added container-fluid class to generic section

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<?php if ( have_posts() ) { ?>
 		<?php while ( have_posts() ) { the_post(); ?>
 
-			<section id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<section id="post-<?php the_ID(); ?>" <?php post_class( 'container-fluid' ); ?>>
 				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 				<div class="entry-content">
