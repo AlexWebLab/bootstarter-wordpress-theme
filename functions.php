8b5324efba26b4b83e254f06c9d3acf8dbde8a72
Alessandro <info@alexweblab.com>
2017-08-08 17:11:03 +0800

    remove wp emoji for performance

--- functions.php
+++ functions.php
@@ -199,3 +199,9 @@ function add_editor_extra_capabilities() {
 	}
 }
 add_action( 'admin_init', 'add_editor_extra_capabilities' );
+
+// remove wp emoji
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
